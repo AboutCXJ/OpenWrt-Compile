c5630bb19e18c8c40c29ef75863e3018e0f3ae70
AboutCXJ <AboutCXJ@gmail.com>
2021-10-25 22:56:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i 's/Mediatek MT7628AN evaluation board/mtk-apsoc-demo/g'  target/linux/ramips/dts/mt7628an_mediatek_mt7628an-eval-board.dts
